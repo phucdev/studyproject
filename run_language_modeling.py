a0be9d8aa3d9ac9f52b181901dec26d7d57e0b9d
Phuc Tran Truong <ptruong@outlook.de>
2023-10-17 18:44:44 +0200

    Refactor run_language_modeling.py to use classic CLI approach instead of fire and add tracking with wandb

--- run_language_modeling.py
+++ run_language_modeling.py
@@ -1,12 +1,12 @@
-import sys
+import argparse
 import os
 import math
 import json
 import logging
 
-import fire
 import datasets
 import torch
+import wandb
 
 from tqdm.auto import tqdm
 from torch.utils.data import DataLoader
@@ -25,59 +25,187 @@ logging.basicConfig(
     level=logging.INFO,
 )
 logger = logging.getLogger(__name__)
+wandb.login()
 
 
-def run_clm(
-    model_name_or_path,
-    output_dir,
-    train_file=None,
-    validation_file=None,
-    dataset_name=None,
-    dataset_config_name=None,
-    validation_split_percentage=10,
-    max_train_samples=None,
-    max_eval_samples=None,
-    per_device_train_batch_size=8,
-    per_device_eval_batch_size=8,
-    learning_rate=3e-4,
-    full_training_learning_rate=1e-4,
-    min_lr=0.0,
-    weight_decay=0.1,
-    gradient_accumulation_steps = 1,
-    pure_embedding_training_percentage=10,
-    warmup_percentage=10,
-    num_train_epochs=1,
-    block_size=None,
-    device="cuda",
-    seed=42
-):
+def parse_args():
+    parser = argparse.ArgumentParser(description="Train a language model.")
+    parser.add_argument(
+        "--model_name_or_path",
+        type=str,
+        help="Path to pretrained model or model identifier from huggingface.co/models.",
+        required=True
+    )
+    parser.add_argument(
+        "--output_dir",
+        type=str,
+        default="output",
+        help="The output directory where the model checkpoints will be written.",
+        required=True
+    )
+    parser.add_argument(
+        "--train_file", type=str, default=None, help="A csv, txt or a json file containing the training data."
+    )
+    parser.add_argument(
+        "--validation_file", type=str, default=None, help="A csv, txt or a json file containing the validation data."
+    )
+    parser.add_argument(
+        "--validation_split_percentage",
+        default=5,
+        help="The percentage of the train set used as validation set in case there's no validation split",
+    )
+    parser.add_argument(
+        "--dataset_name",
+        type=str,
+        default=None,
+        help="The name of the dataset to use (via the datasets library).",
+    )
+    parser.add_argument(
+        "--dataset_config_name",
+        type=str,
+        default=None,
+        help="The configuration name of the dataset to use (via the datasets library).",
+    )
+    parser.add_argument(
+        "--max_train_samples",
+        type=int,
+        default=None,
+        help="For debugging purposes or quicker training, truncate the number of training examples to this value if set."
+    )
+    parser.add_argument(
+        "--max_eval_samples",
+        type=int,
+        default=None,
+        help="For debugging purposes or quicker training, truncate the number of evaluation examples to this value if set."
+    )
+    parser.add_argument(
+        "--per_device_train_batch_size",
+        type=int,
+        default=8,
+        help="Batch size (per device) for the training dataloader.",
+    )
+    parser.add_argument(
+        "--per_device_eval_batch_size",
+        type=int,
+        default=8,
+        help="Batch size (per device) for the evaluation dataloader.",
+    )
+    parser.add_argument(
+        "--learning_rate",
+        type=float,
+        default=5e-5,
+        help="Initial learning rate (after the potential warmup period) to use.",
+    )
+    parser.add_argument(
+        "--full_training_learning_rate",
+        type=float,
+        default=1e-4,
+        help="Initial learning rate (after the potential warmup period) to use for the full training.",)
+    parser.add_argument("--min_lr", type=float, default=0.0, help="Minimum learning rate during training.")
+    parser.add_argument("--weight_decay", type=float, default=0.0, help="Weight decay to use.")
+    parser.add_argument(
+        "--gradient_accumulation_steps",
+        type=int,
+        default=1,
+        help="Number of updates steps to accumulate before performing a backward/update pass.",
+    )
+    parser.add_argument(
+        "--pure_embedding_training_percentage",
+        type=int,
+        default=10,
+        help="Percentage of training steps to train only the embedding layer."
+    )
+    parser.add_argument(
+        "--warmup_percentage",
+        type=int,
+        default=10,
+        help="Percentage of training steps to warmup to the learning rate."
+    )
+    parser.add_argument(
+        "--num_train_epochs",
+        type=int,
+        default=1,
+        help="Total number of training epochs to perform."
+    )
+    parser.add_argument(
+        "--block_size",
+        type=int,
+        default=None,
+        help="Optional input sequence length after tokenization."
+    )
+    parser.add_argument("--device", type=str, default="cuda")
+    parser.add_argument("--seed", type=int, default=42)
+    parser.add_argument("--push_to_hub", action="store_true", help="Whether or not to push the model to the Hub.")
+    parser.add_argument(
+        "--checkpointing_steps",
+        type=str,
+        default=None,
+        help=(
+            "Optional input sequence length after tokenization. The training dataset will be truncated in block of"
+            " this size for training. Default to the model max input length for single sentence inputs (take into"
+            " account special tokens)."
+        ),
+    )
+    parser.add_argument(
+        "--resume_from_checkpoint",
+        type=str,
+        default=None,
+        help="If the training should continue from a checkpoint folder.",
+    )
+    args = parser.parse_args()
+
+    # Sanity checks
+    if args.dataset_name is None and args.train_file is None and args.validation_file is None:
+        raise ValueError("Need either a dataset name or a training/validation file.")
+    else:
+        if args.train_file is not None:
+            extension = args.train_file.split(".")[-1]
+            if extension not in ["csv", "json", "txt"]:
+                raise ValueError("`train_file` should be a csv, json or txt file.")
+        if args.validation_file is not None:
+            extension = args.validation_file.split(".")[-1]
+            if extension not in ["csv", "json", "txt"]:
+                raise ValueError("`validation_file` should be a csv, json or txt file.")
+
+    if args.push_to_hub:
+        if args.output_dir is None:
+            raise ValueError("Need an `output_dir` to create a repo when `--push_to_hub` is passed.")
+
+    return args
+
+
+def run_clm(args):
+    wandb.init(
+        project="CLP study project",
+        config=vars(args),
+    )
     # Load dataset
-    if dataset_name:
+    if args.dataset_name:
         dataset = datasets.load_dataset(
-            dataset_name,
-            dataset_config_name
+            args.dataset_name,
+            args.dataset_config_name
         )
         if "validation" not in dataset.keys():
             dataset["validation"] = datasets.load_dataset(
-                dataset_name,
-                dataset_config_name,
-                split=f"train[:{validation_split_percentage}%]",
+                args.dataset_name,
+                args.dataset_config_name,
+                split=f"train[:{args.validation_split_percentage}%]",
             )
             dataset["train"] = datasets.load_dataset(
-                dataset_name,
-                dataset_config_name,
-                split=f"train[{validation_split_percentage}%:]",
+                args.dataset_name,
+                args.dataset_config_name,
+                split=f"train[{args.validation_split_percentage}%:]",
             )
     else:
         data_files = {}
-        if train_file is not None:
-            data_files["train"] = train_file
-        if validation_file is not None:
-            data_files["validation"] = validation_file
+        if args.train_file is not None:
+            data_files["train"] = args.train_file
+        if args.validation_file is not None:
+            data_files["validation"] = args.validation_file
         extension = (
-            train_file.split(".")[-1]
-            if train_file is not None
-            else validation_file.split(".")[-1]
+            args.train_file.split(".")[-1]
+            if args.train_file is not None
+            else args.validation_file.split(".")[-1]
         )
         if extension == "txt":
             extension = "text"
@@ -89,16 +217,16 @@ def run_clm(
             dataset["validation"] = datasets.load_dataset(
                 extension,
                 data_files=data_files,
-                split=f"train[:{validation_split_percentage}%]"
+                split=f"train[:{args.validation_split_percentage}%]"
             )
             dataset["train"] = datasets.load_dataset(
                 extension,
                 data_files=data_files,
-                split=f"train[{validation_split_percentage}%:]"
+                split=f"train[{args.validation_split_percentage}%:]"
             )
 
     # Preprocess
-    tokenizer = AutoTokenizer.from_pretrained(model_name_or_path)
+    tokenizer = AutoTokenizer.from_pretrained(args.model_name_or_path)
 
     def preprocess_function(examples):
         return tokenizer([x for x in examples["text"]])
@@ -110,7 +238,7 @@ def run_clm(
         remove_columns=dataset["train"].column_names,
     )
 
-    if block_size is None:
+    if args.block_size is None:
         block_size = tokenizer.model_max_length
 
     # Main data processing function that will concatenate all texts from our dataset and generate chunks of block_size.
@@ -120,11 +248,11 @@ def run_clm(
         total_length = len(concatenated_examples[list(examples.keys())[0]])
         # We drop the small remainder, we could add padding if the model supported it instead of this drop, you can
         # customize this part to your needs.
-        if total_length >= block_size:
-            total_length = (total_length // block_size) * block_size
+        if total_length >= args.block_size:
+            total_length = (total_length // args.block_size) * args.block_size
         # Split by chunks of block_size.
         result = {
-            k: [t[i: i + block_size] for i in range(0, total_length, block_size)]
+            k: [t[i: i + args.block_size] for i in range(0, total_length, args.block_size)]
             for k, t in concatenated_examples.items()
         }
         result["labels"] = result["input_ids"].copy()
@@ -132,29 +260,29 @@ def run_clm(
 
     lm_dataset = tokenized_dataset.map(group_texts, batched=True, num_proc=1)
     lm_dataset.set_format("torch")
-    if max_train_samples is not None:
-        max_train_samples = min(len(lm_dataset["train"]), max_train_samples)
+    if args.max_train_samples is not None:
+        max_train_samples = min(len(lm_dataset["train"]), args.max_train_samples)
         lm_dataset["train"] = lm_dataset["train"].select(range(max_train_samples))
-    if max_eval_samples is not None:
-        max_eval_samples = min(len(lm_dataset["validation"]), max_eval_samples)
+    if args.max_eval_samples is not None:
+        max_eval_samples = min(len(lm_dataset["validation"]), args.max_eval_samples)
         lm_dataset["validation"] = lm_dataset["validation"].select(range(max_eval_samples))
 
     tokenizer.pad_token = tokenizer.eos_token
     data_collator = DataCollatorForLanguageModeling(tokenizer=tokenizer, mlm=False)
 
     train_dataloader = DataLoader(
-        lm_dataset["train"], shuffle=True, collate_fn=data_collator, batch_size=per_device_train_batch_size
+        lm_dataset["train"], shuffle=True, collate_fn=data_collator, batch_size=args.per_device_train_batch_size
     )
     eval_dataloader = DataLoader(
-        lm_dataset["validation"], collate_fn=data_collator, batch_size=per_device_eval_batch_size
+        lm_dataset["validation"], collate_fn=data_collator, batch_size=args.per_device_eval_batch_size
     )
 
     # Load pretrained model
-    set_seed(seed)
-    config = AutoConfig.from_pretrained(model_name_or_path)
-    model = AutoModelForCausalLM.from_pretrained(model_name_or_path, config=config)
+    set_seed(args.seed)
+    config = AutoConfig.from_pretrained(args.model_name_or_path)
+    model = AutoModelForCausalLM.from_pretrained(args.model_name_or_path, config=config)
 
-    if pure_embedding_training_percentage > 0:
+    if args.pure_embedding_training_percentage > 0:
         # freeze transformer layer to only train embeddings, the language modeling head (embed_out) is not affected
         for param in model.gpt_neox.parameters():
             param.requires_grad = False
@@ -162,7 +290,7 @@ def run_clm(
         model.gpt_neox.embed_in.weight.requires_grad = True
     transformer_layers_are_frozen = True
 
-    model.to(device)
+    model.to(args.device)
 
     # Optimizer
     # Split weights in two groups, one with weight decay and the other not.
@@ -170,44 +298,44 @@ def run_clm(
     optimizer_grouped_parameters = [
         {
             "params": [p for n, p in model.named_parameters() if not any(nd in n for nd in no_decay)],
-            "weight_decay": weight_decay,
+            "weight_decay": args.weight_decay,
         },
         {
             "params": [p for n, p in model.named_parameters() if any(nd in n for nd in no_decay)],
             "weight_decay": 0.0,
         },
     ]
-    optimizer = torch.optim.AdamW(optimizer_grouped_parameters, lr=learning_rate)
+    optimizer = torch.optim.AdamW(optimizer_grouped_parameters, lr=args.learning_rate)
 
     # LR Scheduler
-    num_update_steps_per_epoch = math.ceil(len(train_dataloader) / gradient_accumulation_steps)
-    num_training_steps = num_train_epochs * num_update_steps_per_epoch
-    pure_embedding_training_steps = math.ceil(num_training_steps * pure_embedding_training_percentage / 100)
-    warmup_embedding_steps = math.ceil(pure_embedding_training_steps * warmup_percentage / 100)
+    num_update_steps_per_epoch = math.ceil(len(train_dataloader) / args.gradient_accumulation_steps)
+    num_training_steps = args.num_train_epochs * num_update_steps_per_epoch
+    pure_embedding_training_steps = math.ceil(num_training_steps * args.pure_embedding_training_percentage / 100)
+    warmup_embedding_steps = math.ceil(pure_embedding_training_steps * args.warmup_percentage / 100)
     warmup_and_embedding_training_steps = warmup_embedding_steps + pure_embedding_training_steps
     # custom lr scheduler with cosine decay and one warmup phase each for the embedding training and the full training
     lr_scheduler = get_custom_lr_scheduler(
         optimizer=optimizer,
-        warmup_percentage=warmup_percentage,
+        warmup_percentage=args.warmup_percentage,
         num_training_steps=num_training_steps,
         num_pure_embedding_training_steps=pure_embedding_training_steps,
-        min_lr=min_lr
+        min_lr=args.min_lr
     )
 
     # Training
-    total_batch_size = per_device_train_batch_size * gradient_accumulation_steps
+    total_batch_size = args.per_device_train_batch_size * args.gradient_accumulation_steps
     logger.info("***** Running training *****")
     logger.info(f"  Num examples = {len(lm_dataset['train'])}")
-    logger.info(f"  Num Epochs = {num_train_epochs}")
-    logger.info(f"  Instantaneous batch size per device = {per_device_train_batch_size}")
+    logger.info(f"  Num Epochs = {args.num_train_epochs}")
+    logger.info(f"  Instantaneous batch size per device = {args.per_device_train_batch_size}")
     logger.info(f"  Total train batch size (w. accumulation) = {total_batch_size}")
-    logger.info(f"  Gradient Accumulation steps = {gradient_accumulation_steps}")
+    logger.info(f"  Gradient Accumulation steps = {args.gradient_accumulation_steps}")
     logger.info(f"  Total optimization steps = {num_training_steps}")
 
     progress_bar = tqdm(range(num_training_steps))
     completed_steps = 0
 
-    for epoch in range(num_train_epochs):
+    for epoch in range(args.num_train_epochs):
         model.train()
         total_loss = 0
         step = 0
@@ -217,7 +345,7 @@ def run_clm(
                 for param in model.gpt_neox.parameters():
                     param.requires_grad = True
                 # set new learning rate for full training and decay to 0.0 for the full training
-                lr_scheduler.base_lrs = [full_training_learning_rate for _ in lr_scheduler.base_lrs]
+                lr_scheduler.base_lrs = [args.full_training_learning_rate for _ in lr_scheduler.base_lrs]
                 lr_scheduler.min_lr = 0.0   # TODO check if we want to decay to 0 for the full training
                 transformer_layers_are_frozen = False
                 logger.info(f"epoch {epoch}: step {completed_steps}: unfreezing transformer layers")
@@ -226,11 +354,16 @@ def run_clm(
             outputs = model(**batch)
             loss = outputs.loss
             # loss = loss_function(outputs, targets)
-            loss = loss / gradient_accumulation_steps
+            loss = loss / args.gradient_accumulation_steps
+            perplexity = math.exp(loss)
             # We keep track of the loss at each epoch
             total_loss += loss.detach().float()
             loss.backward()
-            if (step + 1) % gradient_accumulation_steps == 0:
+            if (step + 1) % args.gradient_accumulation_steps == 0:
+                wandb.log({
+                    "train/train_loss": loss,
+                    "train/perplexity": perplexity,
+                })
                 before_lr = optimizer.param_groups[0]["lr"]
                 optimizer.step()
                 lr_scheduler.step()
@@ -241,7 +374,11 @@ def run_clm(
                 logger.info(f"epoch {epoch}: step {completed_steps}: lr {before_lr} -> {after_lr} loss {loss}")
 
         # gradient accumulation for the last batch
-        if (step + 1) % gradient_accumulation_steps != 0:
+        if (step + 1) % args.gradient_accumulation_steps != 0:
+            wandb.log({
+                "train/train_loss": loss,
+                "train/perplexity": perplexity,
+            })
             before_lr = optimizer.param_groups[0]["lr"]
             optimizer.step()
             lr_scheduler.step()
@@ -269,13 +406,22 @@ def run_clm(
             perplexity = float("inf")
 
         logger.info(f"epoch {epoch}: perplexity: {perplexity} eval_loss: {eval_loss}")
+        wandb.log({
+            "eval/eval_loss": eval_loss,
+            "eval/perplexity": perplexity,
+        })
 
-        model.save_pretrained(output_dir)
-        with open(os.path.join(output_dir, "all_results.json"), "w") as f:
+        model.save_pretrained(args.output_dir)
+        with open(os.path.join(args.output_dir, "all_results.json"), "w") as f:
             json.dump({"perplexity": perplexity}, f)
 
+        wandb.finish()
+
+
+def main():
+    args = parse_args()
+    run_clm(args)
 
 
 if __name__ == '__main__':
-    fire.Fire()
-    sys.exit(0)
+    main()
