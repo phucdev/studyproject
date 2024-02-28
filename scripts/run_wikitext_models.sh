3d7c646da5b63b5c168fcdd3d4189813340cabe0
Phuc Tran Truong <ptruong@outlook.de>
2024-02-28 22:22:29 +0100

    Fix full training lr in constant LRs run command

--- scripts/run_wikitext_models.sh
+++ scripts/run_wikitext_models.sh
@@ -123,7 +123,7 @@ accelerate launch ./run_language_modeling.py \
   --embedding_tuning_warmup_percentage=1 \
   --embedding_tuning_percentage=10 \
   --learning_rate=0.001 \
-  --full_training_learning_rate=0.001 \
+  --full_training_learning_rate=3e-4 \
   --min_lr=3e-5 \
   --lr_scheduler_type="constant_schedule_with_warmup_embedding_tuning" \
   --calculate_warmup_based_on_num_train_steps
